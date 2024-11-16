ff0a176918f6bf227ef6e06df9142309661a0c79
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-16 13:20:17 +0000

    adding age to the string

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -2,7 +2,7 @@
 
 
 function Student({name, age}) {
-   return age < 21 ? <li>Mature Student: {name}</li> : <li>Student name: {name} and age: {age}</li>
+   return age < 21 ? <li>Mature Student: {name} with age of {age}</li> : <li>Student name: {name} and age: {age}</li>
 
     // if(age < 21){
     //     return (
